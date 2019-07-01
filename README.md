31a786ccab246394d77345988f1e957b32e73871
Guillermo Sampallo <guille@gmail.com>
2019-07-01 19:07:23 -0300

    Primer commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # micropython_demo_bot
-Little example of how to create a bot for Telegram
+Little example of how to create a bot for Telegram.
+
+Este es un sencillo ejemplo de como crear un bot de Telegram, utilizando la libreria python-telegram-bot (https://github.com/python-telegram-bot/python-telegram-bot).
+
+El bot esta funcionando, pueden encontrarlo en Telegram como micropython_demo_bot
---
+++ micropython_demo_bot.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# This program is dedicated to the public domain under the CC0 license.
+#
+# THIS EXAMPLE HAS BEEN UPDATED TO WORK WITH THE BETA VERSION 12 OF PYTHON-TELEGRAM-BOT.
+# If you're still using version 11.1.0, please see the examples at
+# https://github.com/python-telegram-bot/python-telegram-bot/tree/v11.1.0/examples
+
+"""
+Simple Bot to reply to Telegram messages.
+
+First, a few handler functions are defined. Then, those functions are passed to
+the Dispatcher and registered at their respective places.
+Then, the bot is started and runs until we press Ctrl-C on the command line.
+
+Usage:
+Basic Echobot example, repeats messages.
+Press Ctrl-C on the command line or send a signal to the process to stop the
+bot.
+"""
+
+import logging
+
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+
+# Define a few command handlers. These usually take the two arguments bot and
+# update. Error handlers also receive the raised TelegramError object in error.
+def start(update, context):
+    """Send a message when the command /start is issued."""
+    update.message.reply_text('Hi!')
+
+
+def help(update, context):
+    """Send a message when the command /help is issued."""
+    update.message.reply_text('Help!')
+
+
+def echo(update, context):
+    """Echo the user message."""
+    update.message.reply_text(update.message.text)
+
+
+def error(update, context):
+    """Log Errors caused by Updates."""
+    logger.warning('Update "%s" caused error "%s"', update, context.error)
+
+def pizza(update, context):
+    if(update.message.text.upper().find("MANZANAS VERDES") > 0):
+        update.message.reply_text("Prefiero comer pizza")
+
+def sumar(update,context):
+    try:
+        numero1 = int(context.args[0])
+        numero2 = int(context.args[1])
+
+        suma = numero1 + numero2
+
+        update.message.reply_text("La suma es "+str(suma))
+
+    except (ValueError):
+        update.message.reply_text("por favor utilice dos numeros")
+
+def main():
+    """Start the bot."""
+    # Create the Updater and pass it your bot's token.
+    # Make sure to set use_context=True to use the new context based callbacks
+    # Post version 12 this will no longer be necessary
+    updater = Updater("848331878:AAFVT1JWTGHIJ9_oaOtHLtCdBPA68CTtCcA", use_context=True)
+
+    # Get the dispatcher to register handlers
+    dp = updater.dispatcher
+
+    # on different commands - answer in Telegram
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("help", help))
+    dp.add_handler(CommandHandler("sumar", sumar))
+
+    # on noncommand i.e message - echo the message on Telegram
+    dp.add_handler(MessageHandler(Filters.text, pizza))
+
+    # log all errors
+    dp.add_error_handler(error)
+
+    # Start the Bot
+    updater.start_polling()
+
+    # Run the bot until you press Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
